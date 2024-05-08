499c1f70d623f59169564a04fc78ae3aefe87f95
adrianarmm <areyemor@myuax.com>
2024-05-08 10:53:10 +0200

    62

--- src/main/java/TWITTER/Utils.java
+++ src/main/java/TWITTER/Utils.java
@@ -1,11 +1,12 @@
 package TWITTER;
 
+
 public class Utils {
     public static boolean isValidAlias(String alias) {
         return alias.matches("[a-zA-Z0-9]+");
     }
 
     public static boolean isValidEmail(String email) {
-        return email.contains("@");
+        return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
     }
 }
