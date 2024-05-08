bff2a7e89e80313e5cfe523c56a1c0d3cc02edfd
adrianarmm <areyemor@myuax.com>
2024-05-08 09:46:06 +0200

    9

--- src/main/java/TWITTER/Tweet.java
+++ src/main/java/TWITTER/Tweet.java
@@ -1,4 +1,13 @@
 package TWITTER;
 
 public class Tweet {
+    private String text;
+
+    public Tweet(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
 }
