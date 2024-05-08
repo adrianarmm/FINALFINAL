d7ad1c36eeffb5e1902c135bf92f710156a52ac9
adrianarmm <areyemor@myuax.com>
2024-05-08 09:59:45 +0200

    19

---
+++ src/main/java/TWITTER/Retweet.java
@@ -0,0 +1,25 @@
+package TWITTER;
+
+import java.time.LocalDate;
+
+public class Retweet extends Tweet {
+    private Tweet originalTweet;
+
+    public Retweet(LocalDate time, String message, CuentaUsuario sender, Tweet originalTweet) {
+        super(time, message, sender);
+        this.originalTweet = originalTweet;
+    }
+
+    public Tweet getOriginalTweet() {
+        return originalTweet;
+    }
+
+    @Override
+    public String toString() {
+        return "Retweet{" +
+                "originalTweet=" + originalTweet.getMessage() +
+                ", message='" + getMessage() + '\'' +
+                ", sender=" + getSender().getAlias() +
+                '}';
+    }
+}
