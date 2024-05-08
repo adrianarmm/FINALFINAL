fcc2206b69d045359579176ebcfc1062b4f9bd09
adrianarmm <areyemor@myuax.com>
2024-05-08 11:46:55 +0200

    83

---
+++ src/main/java/InsertionSort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {50, 20, 84, 13, 22, 16, 89, 85};
+        insertionSort(arr);
+        System.out.println("Sorted array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
