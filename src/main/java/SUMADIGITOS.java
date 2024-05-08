db0f59555dba825915f0c6991e29eecf104dcd13
adrianarmm <areyemor@myuax.com>
2024-05-08 09:33:04 +0200

    2

--- src/main/java/SUMADIGITOS.java
+++ src/main/java/SUMADIGITOS.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class SUMADIGITOS {
+public class SUMADIGITOS {
+    public static void main(String[] args) {
+        int number = 102;
+        int sum = SUMADIGITOS(number);
+        System.out.println("La suma de numeros  " + number + " es " + sum);
+    }
+
+    public static int SUMADIGITOS(int number) {
+        if (number < 10) {
+            return number;
+        } else {
+            return number % 10 + SUMADIGITOS(number / 10);
+        }
+    }
 }
