0df5cd145fb71d0c9c94140fc92d25c2d7eae549
adrianarmm <areyemor@myuax.com>
2024-05-08 09:45:06 +0200

    6

--- src/main/java/TWITTER/CuentaUsuario.java
+++ src/main/java/TWITTER/CuentaUsuario.java
@@ -1,4 +1,50 @@
 package TWITTER;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CuentaUsuario {
-}
+    private String alias;
+    private String email;
+    private List<Tweet> tweets;
+    private List<CuentaUsuario> following;
+    private List<CuentaUsuario> followers;
+    private List<Tweet> timeline;
+
+    public CuentaUsuario(String alias, String email) {
+        this.alias = alias;
+        this.email = email;
+        this.tweets = new ArrayList<>();
+        this.following = new ArrayList<>();
+        this.followers = new ArrayList<>();
+        this.timeline = new ArrayList<>();
+    }
+
+    public void follow(CuentaUsuario user) {
+        if (!following.contains(user)) {
+            following.add(user);
+            user.addFollower(this);
+        }
+    }
+
+    public void addFollower(CuentaUsuario user) {
+        followers.add(user);
+    }
+
+    public void tweet(Tweet tweet) {
+        tweets.add(tweet);
+        for (CuentaUsuario follower : followers) {
+            follower.addToTimeline(tweet);
+        }
+    }
+
+    public void addToTimeline(Tweet tweet) {
+        timeline.add(tweet);
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public String getEmail() {
+        return email;
