191d3bf79c84c4c88d72a870fb82a55f5c89423b
adrianarmm <areyemor@myuax.com>
2024-05-08 11:14:58 +0200

    77

--- src/main/java/TWITTER/Tweet.java
+++ src/main/java/TWITTER/Tweet.java
@@ -2,6 +2,7 @@ package TWITTER;
 public class Tweet {
     private String text;
     private CuentaUsuario user; // The user who created the tweet.
+    private String message;
 
     public Tweet(String text, CuentaUsuario user) {
         this.text = text;
@@ -14,5 +15,13 @@ public class Tweet {
 
     public CuentaUsuario getUser() {
         return user;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
