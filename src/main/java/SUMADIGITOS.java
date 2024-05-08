164cd8d670c60e86dee5001db56bc21eac236b80
adrianarmm <areyemor@myuax.com>
2024-05-08 09:37:03 +0200

    4

--- src/main/java/SUMADIGITOS.java
+++ src/main/java/SUMADIGITOS.java
@@ -1,19 +1,19 @@
 public class SUMADIGITOS {
     public static void main(String[] args) {
-        int number = -102;
+        int number = 102; // Ejemplo de número
         int sum = SUMADIGITOS(number);
-        System.out.println(" La suma de numeros " + number + " es" + sum);
+        System.out.println("La suma de los dígitos del número " + number + " es " + sum);
     }
 
     public static int SUMADIGITOS(int number) {
         if (number < 0) {
-            number = -number;
+            number = -number;  // Convierte el número a positivo si es negativo
         }
 
         if (number < 10) {
-            return number;
+            return number;  // Caso base: si el número tiene un solo dígito, retorna ese dígito
         } else {
-            return number % 10 + SUMADIGITOS(number / 10);
+            return number % 10 + SUMADIGITOS(number / 10);  // Paso recursivo: suma el último dígito y aplica recursividad al número sin el último dígito
         }
     }
 }
