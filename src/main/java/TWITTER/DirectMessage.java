bb8170df5dca751ecc9c7998f855071363b06831
adrianarmm <areyemor@myuax.com>
2024-05-08 10:01:32 +0200

    21

---
+++ src/main/java/TWITTER/DirectMessage.java
@@ -0,0 +1,25 @@
+package TWITTER;
+
+import java.time.LocalDate;
+
+public class DirectMessage extends Tweet {
+    private CuentaUsuario receiver;
+
+    public DirectMessage(LocalDate time, String message, CuentaUsuario sender, CuentaUsuario receiver) {
+        super(time, message, sender);
+        this.receiver = receiver;
+    }
+
+    public CuentaUsuario getReceiver() {
+        return receiver;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectMessage{" +
+                "receiver=" + receiver.getAlias() +
+                ", message='" + getMessage() + '\'' +
+                ", sender=" + getSender().getAlias() +
+                '}';
+    }
+}
