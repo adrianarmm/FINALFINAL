50311708ae335e32e5eb375e36b796225cb0d4b8
adrianarmm <areyemor@myuax.com>
2024-05-08 09:48:05 +0200

    12

---
+++ src/main/java/TWITTER/Email.java
@@ -0,0 +1,12 @@
+package TWITTER;
+public class Email {
+    private String address;
+
+    public Email(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
